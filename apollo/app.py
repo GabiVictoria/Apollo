ef171d74b17f3789ed7b53b037e3f5a014e1375c
Gabi Victoria <gabrielebatista2810@gmail.com>
2024-12-15 07:58:36 -0300

    static

--- apollo/app.py
+++ apollo/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, redirect, url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
+
 
 @app.route('/cadastro')
 def cadastro():
-    return "tudo certo? bem vindo ao apollo"
+    return render_template('cadastro.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
